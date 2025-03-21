495e989ed86c20a6d742e00cdbce5d9c7acc184e
ShawnLay44 <kobe13678010522@163.com>
2025-03-21 15:57:17 +0800

    Add AddTwoNums.py to repository

--- MultiplyThreeNums.py
+++ MultiplyThreeNums.py
@@ -1,3 +1,6 @@
+#Author:Wulei
+#Date:2025/3/21
+#Mutiply three nums
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 c = int(input("Enter the third number: "))
